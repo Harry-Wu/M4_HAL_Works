e4750a0b24633b3da48b29cf6d66a207229feade
Harry <wuxiaohuaer@gmail.com>
2016-11-14 22:47:06 +0800

    ex3-1 V1.0: 用轮询方式收发数据,按键没法起作用.

---
+++ ex3-1 uart receive and send/Inc/led.h
@@ -0,0 +1,10 @@
+#ifndef _LED_H_
+#define _LED_H_
+
+#define BUZZER1  PFout(8)
+#define LED5  PFout(9)
+#define LED6  PFout(10)
+
+//void led_init(void);
+
+#endif
---
+++ ex3-1 uart receive and send/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ex3-1 uart receive and send' 
+ * Target:  'ex3-1 uart receive and send' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
